481ae975e5c566b61201c99234f04ce38799fa77
MegazeroMZ <leanhminh1292@gmail.com>
2025-08-06 21:12:14 -0400

    Update data

--- EventsData.py
+++ EventsData.py
@@ -2,7 +2,7 @@ eventData = [
     ################# EVENTS ####################
     {"name": "At Summer Camp (Year 2)", "options": {"opt1": "Power +10", "opt2": "Guts +10"}},
     {"name": "Dance Lesson", "options": {"opt1": "Power +10", "opt2": "Speed +10"}},
-    {"name": "New Year's Resolutions", "options": {"opt1": "Power +10", "opt2": "Energy +20", "opt3": "Skill points +20"}},
+    {"name": "New Year's Resolutions", "options": {"opt1": "Oguri: Power +10, Rudolf: Wisdom +10", "opt2": "Energy +20", "opt3": "Skill points +20"}},
     {"name": "New Year's Shrine Visit", "options": {"opt1": "Energy +30", "opt2": "All stats +5", "opt3": "Skill points +35"}},
     {"name": "Acupuncture (Just an Acupuncturist, No Worries! ☆)", "options": {"opt1": "30% All stat +20, 70% Mood -2 All stats -15 Night Owl (bad status)", "opt2": "45% Obtain Corner Recovery o + Straightaway Recovery o, 55% Energy -20 Mood -2", "opt3": "70% Maximum Energy +12 Energy +40 Heal all status effect, 30% Energy -20 Mood -2 Practice Poor (bad status)", "opt4": "85% Energy + 20 Mood +1 Charming o (good status), 15% Energy -10/-20 Mood -1 Practice Poor (bad status)", "opt5": "Energy +10"}},
     {"name": "Extra Training", "options": {"opt1": "Last stat +5, Energy -5", "opt2": "Energy +5"}},
--- SupportCardsData.py
+++ SupportCardsData.py
@@ -31,6 +31,43 @@ supportCardData = [
     {"name": "I'm Not a Cyborg", "options": {"opt1": "Guts +10, Skill points +15", "opt2": "Energy -10, Corner Recovery ○ hint +1"}},
     {"name": "Do No Harm", "options": {"opt1": "Energy -10, Stamina +5, Power +15", "opt2": "Energy +10, Wisdom +5"}},
     {"name": "Orders Must Be Followed", "options": {"opt1": "Focus hint +1", "opt2": "Speed +10, Skill points +15"}},
+    # Mayan Top Gun
+    {"name": "Snack Advice for Mayano!", "options": {"opt1": "Stamina +5, Guts +5, Mayano Top Gun bond +5", "opt2": "Stamina +10, Mayano Top Gun bond +5"}},
+    {"name": "Fashion Advice for Mayano!", "options": {"opt1": "Straightaway Adept hint +1, Mayano Top Gun bond +5", "opt2": "Stamina +10, Mayano Top Gun bond +5"}},
+    # Twin Turbo
+    {"name": "I'm Not Afraid!", "options": {"opt1": "Random:, Speed +10, Twin Turbo bond +5, OR, Energy -10, Speed +10, Event chain ended", "opt2": "Energy +20, Event chain ended"}},
+    {"name": "Can't Catch Me!", "options": {"opt1": "Random:, Speed +15, Leader's Pride hint +3, Twin Turbo bond +5, OR, Energy -10, Speed +10, Event chain ended", "opt2": "Energy +25, Event chain ended"}},
+    {"name": "Turbo Is Strong!", "options": {"opt1": "Random:, Energy -10, Speed +5, Early Lead hint +3, OR, Energy -10, Speed +25, Taking the Lead hint +3, Twin Turbo bond +5", "opt2": "Energy +15, Watchful Eye hint +1"}},
+    {"name": "Just Start Running!", "options": {"opt1": "Mood -1, Speed +20", "opt2": "Energy -10, Power +20"}},
+    {"name": "I'm All Fired Up!", "options": {"opt1": "Energy +15, Twin Turbo bond +5", "opt2": "Early Lead hint +1, Twin Turbo bond +5"}},
+    # Daitaku Helios
+    {"name": "#BFF #Party!", "options": {"opt1": "Power +10, Daitaku Helios bond +5", "opt2": "Speed +10, Daitaku Helios bond +5"}},
+    {"name": "#LOL #Party! #Round2", "options": {"opt1": "Power +10, (random) Speed +10, Straight Descent hint +1/+3, Daitaku Helios bond +5", "opt2": "Energy +20, Watchful Eye hint +1, Daitaku Helios bond +5"}},
+    {"name": "Encounter With the Sun ☆", "options": {"opt1": "Power +10, Daitaku Helios bond +5", "opt2": "Get Hot Topic status, Daitaku Helios bond +5"}},
+    {"name": "Smiles Forever", "options": {"opt1": "Speed +5, Power +10, Daitaku Helios bond +5", "opt2": "Long Shot ○ hint +1, Daitaku Helios bond +5"}},
+    # Nice Nature
+    {"name": "Not like Meow", "options": {"opt1": "Energy +20, Nice Nature bond +5", "opt2": "Energy +10, Wisdom +5, Nice Nature bond +5"}},
+    {"name": "(Delicious) Burden", "options": {"opt1": "Ramp Up hint +1, Nice Nature bond +5", "opt2": "Mood +1, Maximum Energy +4, Nice Nature bond +5"}},
+    # Seiun Sky
+    {"name": "Be Strategic ☆", "options": {"opt1": "Energy +10, Wisdom +5, Seiun Sky bond +5", "opt2": "Skill points +30, Second Wind hint +1, Seiun Sky bond -5, Event chain ended"}},
+    {"name": "Recruiting Cat Catchers", "options": {"opt1": "Energy +10, Wisdom +5, Seiun Sky bond +5", "opt2": "Energy -10, Speed +15, Stamina +5, Seiun Sky bond +5"}},
+    {"name": "Recruiting Advisors", "options": {"opt1": "Wisdom +15, Seiun Sky bond +5", "opt2": "Keeping the Lead hint +1, Seiun Sky bond +5"}},
+    # Rice Shower
+    {"name": "A Page of Flower Shop Assistance", "options": {"opt1": "Mood +2, Rice Shower bond +5", "opt2": "Stamina +10, Rice Shower bond +5"}},
+    {"name": "A Page About Cloudy Weather", "options": {"opt1": "Speed +5, Guts +5, Rice Shower bond +5", "opt2": "Firm Conditions ○ hint +1, Rice Shower bond +5"}},
+    # Fine Motion
+    {"name": "Lovely Training Weather ♪", "options": {"opt1": "Wisdom +5, Skill points +20, Fine Motion bond +5", "opt2": "Speed +10, Stamina +5", "opt3": "Get Practice Perfect ○ status, Fine Motion bond +5"}},
+    {"name": "Wonderful New Shoes", "options": {"opt1": "Speed +5, Skill points +10, Fine Motion bond +5", "opt2": "Energy -10, Stamina +5, Skill points +20, Fine Motion bond +5"}},
+    {"name": "Reminiscent Clover", "options": {"opt1": "Corner Adept ○ hint +1, Fine Motion bond +5", "opt2": "Guts +15, Fine Motion bond +5"}},
+    # Agnes Tachyon
+    {"name": "The Correlation between Sleep and Efficiency", "options": {"opt1": "Power +5, Wisdom +5, Agnes Tachyon bond +5", "opt2": "Wisdom +10, Agnes Tachyon bond +5"}},
+    {"name": "Happenstance Introduced Through Intervention", "options": {"opt1": "Late Surger Savvy ○ hint +1, Agnes Tachyon bond +5", "opt2": "Wisdom +10, Agnes Tachyon bond +5"}},
+    # Symboli Rudolf
+    {"name": "The Emperor's Encouragement", "options": {"opt1": "Speed +10", "opt2": "Energy -10, Skill points +30"}},
+    {"name": "The Student Council President's Thoughtfulness", "options": {"opt1": "Rainy Days ○ hint +1", "opt2": "Stamina +15"}},
+    # Hishi Amazon
+    {"name": "Hishiama's Struggles: Problem Children", "options": {"opt1": "Energy +10, Wisdom +5, Hishi Amazon bond +5", "opt2": "Energy -10, Speed +10, Guts +5, Hishi Amazon bond +5"}},
+    {"name": "Hishiama's Struggles: Final Stretch", "options": {"opt1": "Hesitant End Closers hint +1, Hishi Amazon bond +5", "opt2": "Power +5, Skill points +15, Hishi Amazon bond +5"}},
     # Placeholders
     {"name": "aaaaaa", "options": {"opt1": "aaaaaa", "opt2": "aaaaaa"}},
 ]
